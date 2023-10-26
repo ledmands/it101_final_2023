6651c369784bd043a4f9fd1910e43c6f058ec8a5
ledmands <61285955+ledmands@users.noreply.github.com>
2023-10-26 10:54:16 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+# DRIVER FILE FOR THE WEB SERVER
+
+from flask import Flask, render_template, request
+from gpiozero import LED
+# from adafruit_LCD1602 import Adafruit_CharLCD # important that files are in same directory as app file
+# from PCF8574 import PCF8574_GPIO 
+
