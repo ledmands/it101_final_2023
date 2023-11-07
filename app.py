6792fec44ec1dc2876761f612d499207d019ed54
ledmands <61285955+ledmands@users.noreply.github.com>
2023-11-07 14:22:16 -0500

    added functionality for a toggled dark mode with a photoresistor

--- app.py
+++ app.py
@@ -5,20 +5,49 @@ from gpiozero import LED, MotionSensor
 from flask_socketio import SocketIO, send, emit
 from threading import Lock
 from datetime import datetime
-from time import time, ctime
+from time import time, ctime, sleep
+from ADCDevice import *
+
 # from adafruit_LCD1602 import Adafruit_CharLCD # important that files are in same directory as app file
 # from PCF8574 import PCF8574_GPIO 
 async_mode = None
 
 led = LED(17)
 pir_unit = MotionSensor(4)
+adc = ADCDevice() # Define an ADCDevice class object
 app = Flask(__name__)
 socketio = SocketIO(app)
-thread = None
+pir_thread = None
+ldr_thread = None
 thread_lock = Lock()
 
+def adc_setup():
+    global adc
+    if(adc.detectI2C(0x48)): # Detect the pcf8591.
+        adc = PCF8591()
+    elif(adc.detectI2C(0x4b)): # Detect the ads7830
+        adc = ADS7830()
+    else:
+        print("No correct I2C address found, \n"
+        "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
+        "Program Exit. \n");
+        exit(-1)
 
-def background_thread():
+def ldr_background_thread():
+    while True:
+        value = adc.analogRead(0) # read the ADC value of channel 0
+        # led.value = value / 255.0 # Mapping to PWM duty cycle
+        voltage = value / 255.0 * 3.3
+        if value > 100:
+            led.on()
+            socketio.emit('set_darkmode')
+        else:
+            socketio.emit('set_lightmode')
+            led.off()
+        # print ('ADC Value : %d, Voltage : %.2f'%(value,voltage))
+        sleep(0.1)
+
+def pir_background_thread():
     motion_event_counter = 0
     while True:
         pir_unit.wait_for_motion()
@@ -53,12 +82,17 @@ def pir():
     
 @socketio.event
 def connect():
-    global thread
+    global pir_thread
+    global ldr_thread
     with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(background_thread)            
+        if pir_thread is None:
+            pir_thread = socketio.start_background_task(pir_background_thread)  
+        if ldr_thread is None:
+            ldr_thread = socketio.start_background_task(ldr_background_thread)  
+
 #     socketio.emit('log_motion', "connection") # good for debugging or showing initial connection
 
 if __name__ == '__main__':                 # this block runs the web server and the app    
     # app.run(debug=True, host='0.0.0.0')    # 0.0.0.0 host means web app is accessible to any device on network
+    adc_setup()
     socketio.run(app, allow_unsafe_werkzeug=True, debug=True, host='0.0.0.0')
