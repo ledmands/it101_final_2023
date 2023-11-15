b5d6a4cffa94d0b2bd4b93232c56bb57dbd85430
ledmands <61285955+ledmands@users.noreply.github.com>
2023-11-15 13:17:54 -0500

    moved servo code to bottom of app.py file, to be fixed

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # DRIVER FILE FOR THE WEB SERVER
-import os
 
 from flask import Flask, render_template, request
 from gpiozero import LED, MotionSensor, AngularServo
@@ -15,28 +14,12 @@ import Freenove_DHT as DHT
 # from adafruit_LCD1602 import Adafruit_CharLCD # important that files are in same directory as app file
 # from PCF8574 import PCF8574_GPIO 
 async_mode = None
-os.system("sudo pigpiod")
 
 
 led = LED(17)
 pir_unit = MotionSensor(4)
 adc = ADCDevice() # Define an ADCDevice class object
-# servo_pin = 18
 
-
-myGPIO=18
-SERVO_DELAY_SEC = 0.001 
-myCorrection=0.0
-maxPW=(2.5+myCorrection)/1000
-minPW=(0.5-myCorrection)/1000
-# servo =  AngularServo(myGPIO,initial_angle=0,min_angle=0, max_angle=180,min_pulse_width=minPW,max_pulse_width=maxPW)
-servo_on = False
-
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(servo_pin, GPIO.OUT)
-
-# servo_pwm = GPIO.PWM(18, 50)
-# servo_pwm.start(0)
 
 
 app = Flask(__name__)
@@ -60,8 +43,7 @@ def adc_setup():
 def ldr_background_thread():
     while True:
         value = adc.analogRead(0) # read the ADC value of channel 0
-        # led.value = value / 255.0 # Mapping to PWM duty cycle
-        voltage = value / 255.0 * 3.3
+        # voltage = value / 255.0 * 3.3
         if value > 100:
             socketio.emit('set_darkmode', 'dark mode')
         else:
@@ -75,17 +57,13 @@ def pir_background_thread():
         pir_unit.wait_for_motion()
         motion_event_counter += 1
         event_date = datetime.now().strftime("%-d %b %Y")
-        event_time = datetime.now().strftime("%X %p")
-        # socketio.emit('log_motion', {
-        #     'time': ctime(time()), 'event_counter': motion_event_counter
-        #     })        
+        event_time = datetime.now().strftime("%X %p")       
         socketio.emit('log_motion', {
             'date': event_date,
             'time': event_time, 
             'event_counter': motion_event_counter
             })
         pir_unit.wait_for_no_motion()
-
                 
         
 @app.route('/')                           # determines entry point (/ is root)
@@ -108,35 +86,7 @@ def pir():
 def dht():
     return render_template('dht.html')
    
-@app.route('/servo/', methods=['POST'])
-def servo_page():
 
-    return render_template('servo.html')
-
-# @app.route('/servo/test', methods=['POST'])
-# def servo_test():
-#     slider1 = request.form["slider1"]
-#     servo_pwm.ChangeDutyCycle(float(slider1))
-#     sleep(1)
-#     servo_pwm.ChangeDutyCycle(0)
-#     return render_template('servo.html')
-
-
-@socketio.event
-def toggle_servo(is_running):
-    # global servo
-    # global servo_on
-    socketio.emit('check_is_running', is_running)
-    while True:
-        for angle in range(0, 181, 1):   # make servo rotate from 0 to 180 deg
-            servo.angle = angle
-            sleep(SERVO_DELAY_SEC)
-        sleep(0.5)
-        for angle in range(180, -1, -1): # make servo rotate from 180 to 0 deg
-            servo.angle = angle
-            sleep(SERVO_DELAY_SEC)
-        sleep(0.5)
-    
 
 @socketio.event
 def update_dht_clicked():
@@ -170,3 +120,55 @@ if __name__ == '__main__':                 # this block runs the web server and 
     # app.run(debug=True, host='0.0.0.0')    # 0.0.0.0 host means web app is accessible to any device on network
     adc_setup()
     socketio.run(app, allow_unsafe_werkzeug=True, debug=True, host='0.0.0.0')
+
+
+
+
+
+###################################################################
+### UNUSED SERVO CODE, HALF FUNCTIONAL ###
+###################################################################
+
+# servo_pin = 18
+
+# myGPIO=18
+# SERVO_DELAY_SEC = 0.001 
+# myCorrection=0.0
+# maxPW=(2.5+myCorrection)/1000
+# minPW=(0.5-myCorrection)/1000
+# servo =  AngularServo(myGPIO,initial_angle=0,min_angle=0, max_angle=180,min_pulse_width=minPW,max_pulse_width=maxPW)
+
+# servo_on = False
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(servo_pin, GPIO.OUT)
+
+# servo_pwm = GPIO.PWM(18, 50)
+# servo_pwm.start(0)
+
+# @app.route('/servo/test', methods=['POST'])
+# def servo_test():
+#     slider1 = request.form["slider1"]
+#     servo_pwm.ChangeDutyCycle(float(slider1))
+#     sleep(1)
+#     servo_pwm.ChangeDutyCycle(0)
+#     return render_template('servo.html')
+
+
+# @socketio.event
+# def toggle_servo(is_running):
+#     socketio.emit('check_is_running', is_running)
+#     while True:
+#         for angle in range(0, 181, 1):   # make servo rotate from 0 to 180 deg
+#             servo.angle = angle
+#             sleep(SERVO_DELAY_SEC)
+#         sleep(0.5)
+#         for angle in range(180, -1, -1): # make servo rotate from 180 to 0 deg
+#             servo.angle = angle
+#             sleep(SERVO_DELAY_SEC)
+#         sleep(0.5)
+    
+# @app.route('/servo/', methods=['POST'])
+# def servo_page():
+
+#     return render_template('servo.html')
